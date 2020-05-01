1476bf090adc1835c9e103b934cca06752ae2987
David Yin <David Yin@LAPTOP-R2H7D6M9>
2020-04-30 20:41:26 -0700

    stuff

--- src/application/DataStructureADT.java
+++ src/application/DataStructureADT.java
@@ -9,19 +9,19 @@ import java.util.List;
  */
 public interface DataStructureADT {
 
-	// Adds data to the data structure and increase the number of nodes.
-	// If data is null, throw IllegalNullKeyException;
-	// If data is already in data structure, replace value with new value
-	void insert(List<String> data) throws IllegalNullKeyException;
+    // Adds data to the data structure and increase the number of nodes.
+    // If data is null, throw IllegalNullKeyException;
+    // If data is already in data structure, replace value with new value
+    void insert(List<String> data) throws IllegalNullKeyException;
 
 
-	// Returns a list of the data in that node
-	// Does not remove key or decrease number of keys
-	// If index is not found, throw KeyNotFoundException().
-	List<String> get(int index) throws IllegalNullKeyException, KeyNotFoundException;
+    // Returns a list of the data in that node
+    // Does not remove key or decrease number of keys
+    // If index is not found, throw KeyNotFoundException().
+    List<String> get(int index) throws IllegalNullKeyException, KeyNotFoundException;
 
 
-	// Returns the number of nodes in the data structure
-	int size();
+    // Returns the number of nodes in the data structure
+    int size();
 
 }
